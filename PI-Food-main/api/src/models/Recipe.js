9a32be8691925024eb8da8a8041d4c9f1360fb99
BCaneteBalestrelli <benjaminbalestrelli@gmail.com>
2023-06-17 19:53:59 -0300

    Al 17, no funciona la API

--- PI-Food-main/api/src/models/Recipe.js
+++ PI-Food-main/api/src/models/Recipe.js
@@ -4,9 +4,29 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    image: {
+      type: DataTypes.STRING,
+      validate: { isUrl: true },
+    },
+    sumary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    healthScore: {
+      type: DatatTypes.INTEGER,
+    },
+    step: {
+
+    },
+
+  }, {timestamps: false});
 };
